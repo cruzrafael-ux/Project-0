1338234789bd432709a814c09c303ba1d1296a02
RafaelC-tech <143412031+RafaelC-tech@users.noreply.github.com>
2026-06-09 14:01:38 -0400

    Fixing up Template

---
+++ Project 0/src/ShoppingApp.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+import './ShoppingApp.css'
+
+export default ShoppingApp
+
+function ShoppingApp() {
+  
+  return (
+    <header>
+      <h1>Hello World</h1>
+    </header>
+  )
+}
